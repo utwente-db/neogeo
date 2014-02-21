c31e913dfa271a6cf54433604e9002c95d913e68
wombach <a.wombacher@utwente.nl>
2014-02-21 16:24:19 +0100

    small changes; added a legend

--- pre-aggregate-demo/WebContent/js/map.js
+++ pre-aggregate-demo/WebContent/js/map.js
@@ -118,11 +118,11 @@ function init() {
 			controls: [],
 			projection: "EPSG:3857",
 			units: 'm',
-			center: new OpenLayers.LonLat(-0.1, 51)
+			center: new OpenLayers.LonLat(5.8, 52.2)
 	// Google.v3 uses web mercator as projection, so we have to
 	// 	transform our coordinates
 	.transform('EPSG:4326', 'EPSG:3857'),
-	zoom: 5
+	zoom: 7
 	};
 
 
